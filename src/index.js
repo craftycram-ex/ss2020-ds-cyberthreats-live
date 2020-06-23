0d052fc107721ecd41aca9b7fc15970f66ee70ad
craftycram <github@craftycram.net>
2020-06-23 20:47:49 +0200

    Revert "added webserver"
    This reverts commit 88cb285301bc9900c018f538b3752722dcc1dc53.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,24 @@
 const axios = require('axios');
-const WebSocket = require('ws');
-const cors = require('cors');
-
-const wss = new WebSocket.Server({
-  port: 3000
-});
-let websocketConnection;
 
 const dataUrl = 'https://www.fireeye.com/content/dam/legacy/cyber-map/weekly_sanitized.min.js';
 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log('received: %s', message);
-  });
-  //websocketConnection = ws;
-  async function getData() {
-    const response = await axios.get(dataUrl);
-    return response.data;
-  }
-  async function printAttack() {
-    const maxDelayInSec = 5;
-    const data = await getData();
-    const {
-      attacks,
-    } = data;
-    const randTime = Math.random() * 1000 * maxDelayInSec;
-    const randItem = Math.floor(Math.random() * attacks.length);
-    const attack = attacks[randItem];
-    console.log('\n#################################################');
-    console.log('T', randTime, 'A', randItem);
-    console.log(attack);
-    console.log(`Attack from ${attack.OriginCode} to ${attack.Destination}`);
-    console.log('#################################################');
-    ws.send(JSON.stringify(attack));
-    setTimeout(printAttack, randTime);
-  }
-  printAttack();
-});
+async function getData() {
+  const response = await axios.get(dataUrl);
+  return response.data;
+}
+
+async function printAttack() {
+  const maxDelayInSec = 5;
+  const data = await getData();
+  const { attacks } = data;
+  const randTime = Math.random() * 1000 * maxDelayInSec;
+  const randItem = Math.floor(Math.random() * attacks.length);
+  const attack = attacks[randItem];
+  console.log('\n#################################################');
+  console.log('T', randTime, 'A', randItem);
+  console.log(attack);
+  console.log(`Attack from ${attack.OriginCode} to ${attack.Destination}`);
+  console.log('#################################################');
+  setTimeout(printAttack, randTime);
+}
+printAttack();
